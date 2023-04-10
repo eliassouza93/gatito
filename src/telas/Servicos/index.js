799a63506c93160aaa443f06a893e5231b6bd2f1
Elias de Souza <elias99-souza@hotmail.com>
2023-04-10 09:54:40 -0300

    listaRender

---
+++ src/telas/Servicos/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, FlatList } from 'react-native'
+
+const servicos = [
+    {
+        id: 1,
+        nome: 'Banho',
+        preco: 79.9,
+        descricao: 'NÃO DE BANHO NO SEU GATO! mas se precisar nós damos'
+    },
+    {
+        id: 2,
+        nome: 'Vacina v4',
+        preco: 89.9,
+        descricao: 'Uma dose da vaciana v4. Seu gato precisa de duas'
+    },
+    {
+        id: 3,
+        nome: 'Vacina Antirrábica',
+        preco: 99.9,
+        descricao: 'Uma dose da vacina antirrábica. Seu gato precisa de uma por ano'
+    }
+]
+
+export default function () {
+    return <SafeAreaView>
+        <StatusBar />
+        <Text>Serviços!</Text>
+        <FlatList
+            data={servicos}
+            renderItem={({ item }) => <Text> {item.nome}</Text>}
+            keyExtractor={(id) => String(id)} />
+    </SafeAreaView>
+}
